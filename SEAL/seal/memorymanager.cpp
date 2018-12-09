b425cdefc4b1e5776385166b21035642ee776821
Doron <doron.ben.elazar@oracle.com>
2018-12-09 20:56:08 +0200

    Removed old garbage

--- SEAL/seal/memorymanager.cpp
+++
@@ -1,9 +0,0 @@
-#include "seal/memorymanager.h"
-
-namespace seal
-{
-    std::unique_ptr<MMProf>
-        MemoryManager::mm_prof_{ new MMProfGlobal };
-
-    std::mutex MemoryManager::switch_mutex_;
-}

45851f139eced5e8c917658840320ae853e22265
Doron Ben Elazar <doron.ben.elazar@oracle.com>
2019-02-02 13:10:04 +0200

    Verbosity

--- src/Context.cpp
+++ src/Context.cpp
@@ -1,5 +1,9 @@
 #include "Context.h"
 #include "EncryptedArray.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 Context::Context(FHEcontext *context, FHESecKey *secKey): 
     context(*context), secKey(*secKey), modulo(context->alMod.getPPowR()) {
@@ -8,11 +12,17 @@ Context::Context(FHEcontext *context, FHESecKey *secKey):
 
 CtxtExt Context::encrypt(const vector<long>& v) {
     const FHEPubKey& pubKey = secKey;
+    cout << "Creating an encrypted array..." << endl;
     EncryptedArray ea(pubKey.getContext());
     
+    cout << "Duplicating original vector..." << endl;
     vector<long> tmp(v);
     tmp.resize(ea.size(), 0);
+
+    cout << "Creating a ciphertext..." << endl;
     CtxtExt res(pubKey);
+
+    cout << "Encrypting temporary vector" << endl;
     ea.encrypt(res, pubKey, tmp);
     return res;
 }
